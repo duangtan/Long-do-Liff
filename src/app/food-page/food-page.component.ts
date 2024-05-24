c0ae071dcd31bf01bcdc143c3a82a7d99723e03e
duangtan <duangta_n@silpakorn.edu>
2024-05-24 10:09:52 +0700

    fourth

--- src/app/food-page/food-page.component.ts
+++ src/app/food-page/food-page.component.ts
@@ -1,23 +1,53 @@
 import { Component, OnInit } from '@angular/core';
 import { ServiceService } from '../service.service';
 
+export interface foodlist{
+  title : string;
+  difficulty : string;
+  img_food : any;
+}
+
 @Component({
   selector: 'app-food-page',
   templateUrl: './food-page.component.html',
   styleUrls: ['./food-page.component.css']
 })
 export class FoodPageComponent implements OnInit {
-  food: any;
+  food_list: foodlist[] = []; 
 
   constructor(private apiService: ServiceService) { }
 
   ngOnInit(): void {
     this.apiService.getVegFood().subscribe((data: any) => {
-      this.food = data; // สมมติว่า API ตอบกลับข้อมูลเป็น JSON
-      console.log(this.food);
+      this.food_list = data; 
+      console.log(this.food_list);
     }, error => {
       console.error(error);
     });
+  }
+  foodData : any[] = [];
+  getFoodData(): void {
+    this.apiService.getVegFood().subscribe(
+      (data) => {
+        this.foodData.push(data);
+        this.convertData(this.foodData);
+        this.food_list = this.convertData(this.foodData); 
+        console.log("This",this.food_list);    
+      },
+    );
+  }
+  convertData(data:any):any[]{
+    let _data =[];
+    for(let i=0;i<data.length;i++){
+      _data.push({
+        'title': (data[i]['title']||""),
+        'difficulty': (data[i]['difficulty']||""),
+        'img_food' : (data[i]['image']),
+      })
+      console.log("This",data.length);
+      
+    }
+    return _data;
   }
 
 }
