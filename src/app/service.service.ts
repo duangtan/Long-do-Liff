f34981d9a7315ce6faaf0b724d11ad3fa3fa05e1
duangtan <duangta_n@silpakorn.edu>
2024-05-24 12:17:44 +0700

    edit keyapi

--- src/app/service.service.ts
+++ src/app/service.service.ts
@@ -8,14 +8,15 @@ export class ServiceService {
 
   constructor(private http: HttpClient) { }
   getVegFood() {
-    // const url = 'https://the-vegan-recipes-db.p.rapidapi.com/';
-    const url = 'https://the-vegan-recipes-db.p.rapidapi.com/45';
+    const url = 'https://the-vegan-recipes-db.p.rapidapi.com/';
+  
     const headers = {
       // 'X-RapidAPI-Key': '29c5b6aea2msh1c59e5a232dfdcdp1dfbd4jsn091d2c13df9f',
       // 'X-RapidAPI-Host': 'the-vegan-recipes-db.p.rapidapi.com'
       'X-RapidAPI-Key': 'da06a436f8msha76c0f760f12566p197ae7jsnf5057645a4b0',
 		  'X-RapidAPI-Host': 'the-vegan-recipes-db.p.rapidapi.com'
     };
+    
 
     return this.http.get(url, { headers });
   }
