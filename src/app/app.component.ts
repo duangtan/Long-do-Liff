6dc8f2e13e44e27b4b7acebefdd4f1c151316b77
duangtan <duangta_n@silpakorn.edu>
2024-05-24 08:12:15 +0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,51 @@
-import { Component } from '@angular/core';
+// import { Component } from '@angular/core';
+
+// @Component({
+//   selector: 'app-root',
+//   templateUrl: './app.component.html',
+//   styleUrls: ['./app.component.css']
+// })
+// export class AppComponent {
+//   title = 'long-do-liff';
+// }
+
+import { Component, OnInit } from '@angular/core';
+import liff from '@line/liff';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'long-do-liff';
+export class AppComponent implements OnInit {
+  title = 'my-liff-app';
+
+  ngOnInit() {
+    this.initializeLiff();
+  }
+
+  initializeLiff() {
+    liff.init({ liffId: 'YOUR_LIFF_ID' })
+      .then(() => {
+        if (liff.isLoggedIn()) {
+          this.getProfile();
+        } else {
+          liff.login();
+        }
+      })
+      .catch((err: any) => {
+        console.error('LIFF Initialization failed', err);
+      });
+  }
+
+  getProfile() {
+    liff.getProfile()
+      .then(profile => {
+        console.log(profile);
+        // เพิ่มโค้ดเพื่อแสดงโปรไฟล์หรือใช้ข้อมูลโปรไฟล์ที่ได้ตามต้องการ
+      })
+      .catch((err: any) => {
+        console.error('Error getting profile', err);
+      });
+  }
 }
