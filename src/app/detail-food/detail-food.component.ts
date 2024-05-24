f0f2e2ed5d7b2dde50db8bdce72e3e56dbbca03e
duangtan <duangta_n@silpakorn.edu>
2024-05-24 17:01:48 +0700

    edit again

--- src/app/detail-food/detail-food.component.ts
+++ src/app/detail-food/detail-food.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
-import { ServiceService } from '../service.service';
+import { ActivatedRoute } from '@angular/router';
 
 export interface fooddetail{
   title : string;
@@ -20,51 +19,72 @@ export interface fooddetail{
   styleUrls: ['./detail-food.component.css']
 })
 export class DetailFoodComponent implements OnInit {
-  food_detail: fooddetail[] = []; 
-  constructor(private apiService: ServiceService,private router: Router) { }
+  foodId: string | undefined;
+
+  constructor(private route: ActivatedRoute) { }
+
   ngOnInit(): void {
-    this.apiService.getVegFood().subscribe((data: any) => {
-      this.food_detail = data; 
-      console.log("FoodIDdetail",this.food_detail);
-    }, error => {
-      console.error(error);
+    this.route.params.subscribe(params => {
+      this.foodId = params['foodId'];
+      console.log('Food Id:', this.foodId);
+      // สามารถใช้ this.foodId ในการเรียก API หรือประมวลผลข้อมูลต่อไปได้
     });
   }
-  FoodDetailData : any[] = [];
-  getFoodDetailData(FoodID: string): void {
-    this.apiService.getFoodById(FoodID).subscribe(
-      (data) => {
-        this.FoodDetailData.push(data);
-        this.convertData(this.FoodDetailData);
-        this.food_detail = this.convertData(this.FoodDetailData);     
-      },
-    );
-  }
-  convertData(data:any):any[]{
-    let _datadetail =[];
-    for(let i=0;i<data.length;i++){
-      _datadetail.push({
-        'title': (data[i]['title']||""),
-        'difficulty': (data[i]['difficulty']||""),
-        'image' : (data[i]['image']||""),
-        'id' : (data[i]['id']||""),
-        'portion' : (data[i]['portion']||""),
-        'time' : (data[i]['time']||""),
-        'description' : (data[i]['description']||""),
-        'ingredients' : (data[i]['ingredients']||""),
-        'method' : (data[i]['method']||""),
-      })
-      console.log("This",data.length);
-      
-    }
-    return _datadetail;
-  }
-
   test(){
-    console.log("FoodID",this.food_detail);
-    
+    console.log("FoodID",this.foodId);
     
     
   }
 
+  // recipe = {
+  //   id: "45",
+  //   title: "Smoky chipotle tofu with tomatoes and avocado",
+  //   difficulty: "Easy",
+  //   portion: "Serves 2-3",
+  //   time: "Hands-on time 15 min. Simmering time 7-8 min",
+  //   description: "Pair warm, intensely smoky, chipotle tofu with soothing smashed avocado. This vegan combo is incredible on toast or in breakfast tacos.",
+  //   ingredients: [
+  //     "2 tbsp olive oil",
+  //     "225g block smoked firm tofu, cut into small cubes",
+  //     "3 tbsp chipotles in adobo",
+  //     "½ tsp sea salt",
+  //     "3 spring onions, finely sliced",
+  //     "150g cherry tomatoes, halved",
+  //     "Tacos or toast to serve",
+  //     "1 large avocado",
+  //     "Grated zest and juice 1 lime",
+  //     "Garlic cloves",
+  //     "Chilli oil or Korean chilli flakes",
+  //     "Chopped coriander, chervil, tarragon, basil or dill",
+  //     "Drizzle of tahini"
+  //   ],
+  //   method: [
+  //     {
+  //       "Step 1": "For the smashed avocado, cut the avocado in half and discard the stone. Mash the garlic, if using, with ½ tsp salt using a knife or pestle and mortar. Mash with the avocado, then add the lime zest and juice (and, if using, the chilli oil/flakes, herbs and tahini). Season to taste."
+  //     },
+  //     {
+  //       "Step 2": "Heat a frying pan over a medium-high heat and, when hot, add the olive oil, then the tofu. Fry for a few minutes until crisp, then add the chipotles. Stir-fry for a few minutes, seasoning with the sea salt, then add the spring onions and tomatoes. Turn the heat to medium and cook for 7-8 minutes until the tomatoes begin to break down. Taste and adjust the seasoning, then serve with the smashed avocado, in tacos or on toast."
+  //     }
+  //   ],
+  //   image: "https://apipics.s3.amazonaws.com/vegan_api/45.jpg"
+  // };
+  // convertData(data:any):any[]{
+  //   let _data =[];
+  //   for(let i=0;i<data.length;i++){
+  //     _data.push({
+  //       'title': (data[i]['title']||""),
+  //       'difficulty': (data[i]['difficulty']||""),
+  //       'image' : (data[i]['image']||""),
+  //       'id' : (data[i]['id']||""),
+  //       'portion' : (data[i]['portion']||""),
+  //       'time' : (data[i]['time']||""),
+  //       'description' : string;
+  //       'ingredients' : string;
+  //       'method' : string;
+  //     })
+  //     console.log("This",data.length);
+      
+  //   }
+  //   return _data;
+  // }
 }
