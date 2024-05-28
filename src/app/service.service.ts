64149c1f7f52f86ce3955cd37a32b9a2e60d53a4
duangtan <duangta_n@silpakorn.edu>
2024-05-28 14:14:22 +0700

    edit key

--- src/app/service.service.ts
+++ src/app/service.service.ts
@@ -9,8 +9,8 @@ import { Observable } from 'rxjs';
 export class ServiceService {
   private apiUrl = 'https://the-vegan-recipes-db.p.rapidapi.com/';
   private headers = new HttpHeaders({
-    'X-RapidAPI-Key': '115fee7a62msh208feeaee8a6d08p1f9903jsn542593f71b9e',
-    'X-RapidAPI-Host': 'the-vegan-recipes-db.p.rapidapi.com'
+    'x-rapidapi-key': '64b6ec1b95msh824e810cc0b079dp19a41bjsn746c3081d0f2',
+		'x-rapidapi-host': 'the-vegan-recipes-db.p.rapidapi.com'
 
   });
 
