0ec991a68d4dfda4703d8698078d9baad4ab41c4
duangtan <duangta_n@silpakorn.edu>
2024-05-29 15:57:09 +0700

    edit Home

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home-page',
@@ -6,5 +7,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./home-page.component.css']
 })
 export class HomePageComponent {
+  constructor(private router: Router) {}
+  isVeg : boolean = true
+  isCocktail : boolean = false;
+  
+  viewMenu(){
+    this.router.navigate(['/menu']);
+  }
+  
+  showCocktail = false;
 
+  showVegCard() {
+    this.showCocktail = false;
+  }
+
+  showCocktailCard() {
+    this.showCocktail = true;
+  }
+  
 }
